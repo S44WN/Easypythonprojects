589c63cf31ce36deacebf3adac7347c5ab9e60ab
Sawan <itissaavn@gmail.com>
2022-09-17 10:22:17 +0530

    completd treasure map

--- treasuremap.py
+++ treasuremap.py
@@ -9,4 +9,11 @@ map = [r1, r2, r3, r4, r5]
 
 print(f"{r1}\n{r2}\n{r3}\n{r4}\n{r5}")
 
-position = input('where do u want to put the reasure? : ')
+position = input('where do u want to put the treasure (give xy)? : ')
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+map[vertical-1][horizontal-1] = '📍'
+
+print(f"{r1}\n{r2}\n{r3}\n{r4}\n{r5}")
