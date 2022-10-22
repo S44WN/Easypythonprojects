f8a48cf9c97571c877b08581940ab5567c95c019
Sawan <itissaavn@gmail.com>
2022-10-22 20:39:48 +0530

    init oop

---
+++ oop/car.py
@@ -0,0 +1,9 @@
+class User:
+    pass
+
+
+user_1 = User()
+user_1.first_name = "John"
+user_1.last_name = "Smith"
+
+print(user_1.first_name)
---
+++ quiz-game-start/question_model.py
@@ -0,0 +1,7 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+question_1 = Question('1+1 : ', '2')
